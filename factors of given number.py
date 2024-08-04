bb5115b23a504e52e57d58f3af04d673001af3b7
vikramdesai123 <121470452+vikramdesai123@users.noreply.github.com>
2024-08-04 07:33:40 +0000

    factors of given number

--- factors of given number.py
+++ factors of given number.py
@@ -1,9 +1,30 @@
-def factors(num):
-    fact = []
-    for i in range(1,(num+1)):
-        if num%i == 0:
-            fact.append(i)
-    return fact
+from math import * 
+    # 1st approach
+        
+# def factors(num):
+#     fact = []
+#     for i in range(1,(num+1)):
+#         if num%i == 0:
+#             fact.append(i)
+#     return fact
 
-num = int(input("Enter number to find its factors:"))
-print(factors(num))
+# num = int(input("Enter number to find its factors:"))
+# print(factors(num))
+
+
+    # 2nd approach
+
+def facts(num):
+    i = 1
+    while(i*i < num):
+        if num%i == 0:
+            print(i, end=" ")
+        i = i+1
+    
+    for i in range(int(sqrt(num)), 0, -1):
+        if num % i == 0:
+            print(num//i , end=" ")
+        
+
+num = int(input("enter num to check its factors:"))
+facts(num)
